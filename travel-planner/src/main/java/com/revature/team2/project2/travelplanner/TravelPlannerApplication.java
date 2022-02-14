450ff930e4eb66a75f35e2b3948f24e940d543ee
jondgons <jondgons@gmail.com>
2022-02-14 16:32:16 -0600

    controller to map endpoint

--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/TravelPlannerApplication.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/TravelPlannerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelPlannerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("log message");
 		SpringApplication.run(TravelPlannerApplication.class, args);
 	}
 
