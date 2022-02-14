4a876ca5c87e0e151f1b5eee61d6a2c284d08113
jondgons <jondgons@gmail.com>
2022-02-14 16:53:12 -0600

    lombok + example bean with lombok

---
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/Thing.java
@@ -0,0 +1,20 @@
+package com.revature.team2.project2.travelplanner.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "things")
+public class Thing {
+    @Column(name = "thing_id")
+    private Integer id;
+    
+    @Column(name = "some_string")
+    private String someString;
+}
