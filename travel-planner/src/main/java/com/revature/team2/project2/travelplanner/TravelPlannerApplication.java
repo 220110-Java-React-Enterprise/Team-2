87fc7d07c7959c90bb40a297fb966c49982f93c1
jondgons <jondgons@gmail.com>
2022-02-15 17:03:13 -0600

    scan for beans package

--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/TravelPlannerApplication.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/TravelPlannerApplication.java
@@ -5,7 +5,7 @@ import com.revature.team2.project2.travelplanner.beans.models.Thing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.revature.team2.project2.travelplanner.beans")
 public class TravelPlannerApplication {
 
 	public static void main(String[] args) {
