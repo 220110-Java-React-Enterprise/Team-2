f80139aebfaa9eb862d191023348cd085a92ef1a
Alvin <huerox@msn.com>
2022-02-16 11:08:01 -0800

    began creating the CRUD requests for the user and admin controllers.

--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/AdminController.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/AdminController.java
@@ -1,12 +1,26 @@
 package com.revature.team2.project2.travelplanner.beans.controllers;
 
+import com.revature.team2.project2.travelplanner.beans.respositories.AdminRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Acts as a servlet for HTTP requests
  */
-@RestController
+@RestController // @Response and @Controller are implied by @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    private final AdminRepository adminRepository;
+
+    public AdminController(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    // GET all administrators
+
+    // GET an administrator by ID
+
+    // POST and administrator
+
+    // DELETE an administrator by ID
 }
--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/UsersController.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/UsersController.java
@@ -1,12 +1,26 @@
 package com.revature.team2.project2.travelplanner.beans.controllers;
 
+import com.revature.team2.project2.travelplanner.beans.respositories.UsersRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Acts as a servlet for HTTP requests
  */
-@RestController
+@RestController // @Response and @Controller are implied by @RestController
 @RequestMapping("/users")
 public class UsersController {
+    private UsersRepository usersRepository;
+
+    public UsersController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    // GET all administrators
+
+    // GET an administrator by ID
+
+    // POST and administrator
+
+    // DELETE an administrator by ID
 }
