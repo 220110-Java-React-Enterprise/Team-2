111cf698458d29dc80c8644b86b2bb5cfda1fa2c
Alvin <huerox@msn.com>
2022-02-16 10:11:47 -0800

    stubbed out admin and user controllers.

---
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/AdminController.java
@@ -0,0 +1,12 @@
+package com.revature.team2.project2.travelplanner.beans.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Acts as a servlet for HTTP requests
+ */
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+}
