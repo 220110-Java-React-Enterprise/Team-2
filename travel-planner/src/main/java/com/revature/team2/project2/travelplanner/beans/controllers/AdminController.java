cd0bc1712b98713dcc5be5412d058513f536510d
Alvin <huerox@msn.com>
2022-02-16 11:56:55 -0800

    completed adding the CRUD verb operations for admin controller.

--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/AdminController.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/controllers/AdminController.java
@@ -1,9 +1,13 @@
 package com.revature.team2.project2.travelplanner.beans.controllers;
 
+import com.revature.team2.project2.travelplanner.beans.models.Admin;
 import com.revature.team2.project2.travelplanner.beans.respositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Acts as a servlet for HTTP requests
@@ -19,11 +23,25 @@ public class AdminController {
     }
 
     // GET all administrators
-
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Admin> getAllAdmin(){
+        return adminRepository.findAll();
+    }
 
     // GET an administrator by ID
-
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Admin getAdminById(Integer id){
+        return adminRepository.getById(id);
+    }
     // POST and administrator
+    @RequestMapping(method = RequestMethod.POST)
+    public void postAdmin(Admin admin){
+        adminRepository.save(admin);
+    }
 
     // DELETE an administrator by ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteAdminById(Integer id){
+        adminRepository.deleteById(id);
+    }
 }
