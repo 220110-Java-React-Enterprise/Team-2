d26c425d64eba206f1ab5116b1473a3e97d13aa6
Alvin <huerox@msn.com>
2022-02-16 10:08:38 -0800

    created user and admin models.

--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/respositories/AdminRepository.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/respositories/AdminRepository.java
@@ -1,5 +1,6 @@
 package com.revature.team2.project2.travelplanner.beans.respositories;
 
+import com.revature.team2.project2.travelplanner.beans.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
