db10463760fcec83c1700f116e0a81572fac2054
Alvin <huerox@msn.com>
2022-02-16 09:57:48 -0800

    created users and admin repositories and extended the JpaRepository.

---
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/respositories/AdminRepository.java
@@ -0,0 +1,12 @@
+package com.revature.team2.project2.travelplanner.beans.respositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Handles data persistance CRUD
+ */
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+}
