181c0bd9c4fe0ee72c0bddddc37abcd01e6aa0ce
jondgons <jondgons@gmail.com>
2022-02-17 16:32:38 -0600

    fixing appcontext provider

--- travel-planner/src/main/java/com/revature/team2/project2/travelplanner/TravelPlannerApplication.java
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/TravelPlannerApplication.java
@@ -1,9 +1,14 @@
 package com.revature.team2.project2.travelplanner;
 
+import com.revature.team2.project2.travelplanner.beans.models.Admin;
 import com.revature.team2.project2.travelplanner.beans.models.Thing;
+import com.revature.team2.project2.travelplanner.beans.repositories.AdminRepository;
+import com.revature.team2.project2.travelplanner.beans.repositories.UsersRepository;
+import com.revature.team2.project2.travelplanner.beans.utils.ApplicationContextProvider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "com.revature.team2.project2.travelplanner.beans")
 public class TravelPlannerApplication {
@@ -15,7 +20,13 @@ public class TravelPlannerApplication {
 		//! TESTING STUFF
 		Thing thing = new Thing();
 		System.out.println("thing's id: " + thing.getId());
+
+		ApplicationContext context = ApplicationContextProvider.getApplicationContext();
+
+		AdminRepository adminRepository = context.getBean(AdminRepository.class);
+		UsersRepository usersRepository = context.getBean(UsersRepository.class);
+
+		adminRepository.save(new Admin("jon", "gons", "jons", "pass"));
 	}
 
 }
-
