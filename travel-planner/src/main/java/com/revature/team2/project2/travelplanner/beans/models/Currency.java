286ff0821cdf2dd84b43f8bf3e7a401f4bf8f99e
jondgons <jondgons@gmail.com>
2022-02-22 10:07:49 -0600

    currency response parsing

---
+++ travel-planner/src/main/java/com/revature/team2/project2/travelplanner/beans/models/Currency.java
@@ -0,0 +1,17 @@
+package com.revature.team2.project2.travelplanner.beans.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Currency {
+    private Double new_amount;
+    private String new_currency;
+    private String old_currency;
+    private Double old_amount;
+}
