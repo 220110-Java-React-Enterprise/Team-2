db542bcc266f0a33b08ae2b69127777e0a9f0f39
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2022-02-22 20:45:52 -0700

    js directory, cors filter, currency progress

--- travel-planner/ui/js/currency.js
+++ travel-planner/ui/js/currency.js
@@ -4,31 +4,37 @@ console.log(submitBtn)
 
 submitBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    const params = new URLSearchParams([...new FormData(e.target).entries()]);
+    //const params = new URLSearchParams([...new FormData(e.target).entries()]);
     console.log("button clicked")
-    let origCur = document.getElementById("have").textContent;
+    let origCur = document.getElementById("have").value;
     console.log(origCur)
+    let oldAmt = document.getElementById("amount").value;
+    console.log(oldAmt);
+    let newCur = document.getElementById("want").value;
+    console.log(newCur);
     let url = "http://localhost:8080/api/convert";
-    fetch(url, {
+    let response = fetch(url, {
         headers: {
             'Content_Type': 'application/json;charset=utf-8'
         },
         method: "GET",
-        body: params
+        //body: params
     });
 
     try {
-        console.log("========");
-        let result = response.json();
+        let result = response.json;
         console.log(result);
     } catch (error) {
         console.log(error);
     }
 
 })
-// .then((){
-//     NEED TO DO SOMETHING MORE WITH THE RESPONSE
-// });
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+
+
 
 
 
