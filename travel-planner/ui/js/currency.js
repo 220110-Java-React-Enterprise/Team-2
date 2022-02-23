e64756e57f5e74b7ab45b5e7e0145a6d6eadd66c
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2022-02-23 11:08:29 -0700

    currency WORKS

--- travel-planner/ui/js/currency.js
+++ travel-planner/ui/js/currency.js
@@ -1,10 +1,10 @@
-let submitBtn = document.getElementById("submitButton");
+let submitBtn = document.getElementById("calcButton");
 console.log(submitBtn)
 
 
-submitBtn.addEventListener('click', function (e) {
+submitBtn.addEventListener('click', function(e) {
     e.preventDefault();
-    //const params = new URLSearchParams([...new FormData(e.target).entries()]);
+    const params = new URLSearchParams;
     console.log("button clicked")
     let origCur = document.getElementById("have").value;
     console.log(origCur)
@@ -12,25 +12,29 @@ submitBtn.addEventListener('click', function (e) {
     console.log(oldAmt);
     let newCur = document.getElementById("want").value;
     console.log(newCur);
-    let url = "http://localhost:8080/api/convert";
+    let url = "http://localhost:8080/api/convert?have=" + origCur + "&want=" + newCur + "&amount=" + oldAmt   ;
     let response = fetch(url, {
         headers: {
             'Content_Type': 'application/json;charset=utf-8'
         },
-        method: "GET",
-        //body: params
+        method: "get",
+        
+    }).then(response => response.json())
+    .then(function(data) {
+        console.log(data)
+        document.getElementById("new_amount").append(data)
+
     });
 
-    try {
-        let result = response.json;
-        console.log(result);
-    } catch (error) {
-        console.log(error);
-    }
+    // try {
+    //     let result = response.json;
+    //     console.log(result);
+    // } catch (error) {
+    //     console.log(error);
+    // }
 
 })
-    .then(response => response.json())
-    .then(data => console.log(data));
+    
 
 
 
