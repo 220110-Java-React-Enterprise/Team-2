44b1d0559d77c0292b3d7acda33377cef024e66a
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2022-02-23 17:12:09 -0700

    weather works. not the way be intended. shows only one line of data per day, for 5 days

--- travel-planner/ui/js/weather.js
+++ travel-planner/ui/js/weather.js
@@ -16,24 +16,62 @@ submitButton.addEventListener("click", function(event) {
         .then(function(data) {
             console.log(data)
             document.getElementById("forecastCity").append(cityName)
-          
+            
             console.log(data['list'][0])
-            
-            for (let index = 0; index < data.length; index++) {
-                const element = data[index];
-                document.getElementById("print_fore").append("Date and Time: ")
-                document.getElementById("print_fore").append(data['list'][index]["dt_txt"])
-
-                document.getElementById("print_fore").append("\nMin Temp: ")
-                document.getElementById("print_fore").append(data['list'][index]["tempMin"])
-            
-                document.getElementById("print_fore").append("\nMax Temp: ")
-                document.getElementById("print_fore").append(data['list'][index]["tempMax"]) 
-
-                document.getElementById("print_fore").append("\nHumidity: ")
-                document.getElementById("print_fore").append(data['list'][index]["humidity"])
+                document.getElementById("1").append("Date and Time: ")
+                document.getElementById("1").append(data['list'][0]["dt_txt".slice(0,9)])
+                document.getElementById("1").append("  Min Temp: ")
+                document.getElementById("1").append(data['list'][0]["tempMin"])
+                document.getElementById("1").append("  Max Temp: ")
+                document.getElementById("1").append(data['list'][0]["tempMax"]) 
+                document.getElementById("1").append("  Humidity: ")
+                document.getElementById("1").append(data['list'][0]["humidity"])
+                document.getElementById("1").append("   Weather: ")
+                document.getElementById("1").append(data['list'][4]['weather'][0]["description"])
                 
-            }
+                document.getElementById("2").append("Date and Time: ")
+                document.getElementById("2").append(data['list'][8]["dt_txt"])
+                document.getElementById("2").append("  Min Temp: ")
+                document.getElementById("2").append(data['list'][8]["tempMin"])
+                document.getElementById("2").append("  Max Temp: ")
+                document.getElementById("2").append(data['list'][8]["tempMax"]) 
+                document.getElementById("2").append("  Humidity: ")
+                document.getElementById("2").append(data['list'][8]["humidity"])
+                document.getElementById("2").append("   Weather: ")
+                document.getElementById("2").append(data['list'][4]['weather'][0]["description"])
+
+                document.getElementById("3").append("Date and Time: ")
+                document.getElementById("3").append(data['list'][16]["dt_txt"])
+                document.getElementById("3").append("  Min Temp: ")
+                document.getElementById("3").append(data['list'][16]["tempMin"])
+                document.getElementById("3").append("  Max Temp: ")
+                document.getElementById("3").append(data['list'][16]["tempMax"]) 
+                document.getElementById("3").append("  Humidity: ")
+                document.getElementById("3").append(data['list'][16]["humidity"])
+                document.getElementById("3").append("   Weather: ")
+                document.getElementById("3").append(data['list'][4]['weather'][0]["description"])
+
+                document.getElementById("4").append("Date and Time: ")
+                document.getElementById("4").append(data['list'][24]["dt_txt"])
+                document.getElementById("4").append("  Min Temp: ")
+                document.getElementById("4").append(data['list'][24]["tempMin"])
+                document.getElementById("4").append("  Max Temp: ")
+                document.getElementById("4").append(data['list'][24]["tempMax"]) 
+                document.getElementById("4").append("  Humidity: ")
+                document.getElementById("4").append(data['list'][24]["humidity"])
+                document.getElementById("4").append("   Weather: ")
+                document.getElementById("4").append(data['list'][4]['weather'][0]["description"])
+
+                document.getElementById("5").append("Date and Time: ")
+                document.getElementById("5").append(data['list'][32]["dt_txt"])
+                document.getElementById("5").append("  Min Temp: ")
+                document.getElementById("5").append(data['list'][32]["tempMin"])
+                document.getElementById("5").append("  Max Temp: ")
+                document.getElementById("5").append(data['list'][32]["tempMax"]) 
+                document.getElementById("5").append("  Humidity: ")
+                document.getElementById("5").append(data['list'][32]["humidity"])
+                document.getElementById("5").append("   Weather: ")
+                document.getElementById("5").append(data['list'][4]['weather'][0]["description"])
             
          
         });
